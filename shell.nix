b37d628c587b60f3cdb03652ec1dc11de4486c37
Thoq <tristanshaw000@gmail.com>
2025-10-10 17:08:49 -0400

    remove Dockerfile, update shell.nix

--- shell.nix
+++ shell.nix
@@ -2,10 +2,11 @@
 
 pkgs.mkShell {
   buildInputs = with pkgs; [
-    kotlin
     gradle
   ];
 
+  # intellij sdk manager
+  # nix doesnt have jdk25 yet...
   shellHook = ''
   export JAVA_HOME="$HOME/.jdks/openjdk-25"
   export PATH="$JAVA_HOME/bin:$PATH"
