54541f3c2ca75cfc6884682358d6694bd9a57a40
Thoq <tristanshaw000@gmail.com>
2025-10-20 08:58:14 -0400

    ascii art

--- src/main/java/com/github/kinetic/logthing/Start.java
+++ src/main/java/com/github/kinetic/logthing/Start.java
@@ -14,11 +14,40 @@ public class Start {
     private static final LogUtil log = new LogUtil();
     private static final StdUtil stdUtil = new StdUtil();
 
+    /**
+     * Colors
+     */
+    private static final String MAGENTA = "\033[35m";
+    private static final String RESET = "\033[0m";
+
     public static void main(String[] args) {
         Thread.currentThread().setName("B");
+
+        final String asciiArt = getAsciiArt();
+        System.out.println(asciiArt);
 
         log.debug("Booting...");
 
         LogThing.getInstance().launch(stdUtil.concat(new String[]{""}, args));
+    }
+
+    private static String getAsciiArt() {
+        return MAGENTA + """
+                           LogThing
+                      ,--,           ,----,
+                   ,---.'|         ,/   .`|
+                   |   | :       ,`   .'  :
+                   :   : |     ;    ;     /
+                   |   ' :   .'___,/    ,'
+                   ;   ; '   |    :     |
+                   '   | |__ ;    |.';  ;
+                   |   | :.'|`----'  |  |
+                   '   :    ;    '   :  ;
+                   |   |  ./     |   |  '
+                   ;   : ;       '   :  |
+                   |   ,/        ;   |.'
+                   '---'         '---'
+                      By Kinetic Labs
+                """ + RESET;
     }
 }
